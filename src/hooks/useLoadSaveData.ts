ec78749c4eef6f8a8cd61beed6b89f6a71d471d2
tokkissi <alal4674@gmail.com>
2023-07-10 14:55:31 +0900

    feat: add axios

--- src/hooks/useLoadSaveData.ts
+++ src/hooks/useLoadSaveData.ts
@@ -1,12 +1,20 @@
 import { SaveData } from "@/model/gameData";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function loadSaveDataByEmail(email: string) {
-  return fetch("/api/savedata/load").then((res) => {
-    if (!res.ok) {
-      throw new Error(`api 에러, status code: ${res.status}`);
+  // fetch 에서 axios 로 수정
+  // return fetch("/api/savedata/load").then((res) => {
+  //   if (!res.ok) {
+  //     throw new Error(`api 에러, status code: ${res.status}`);
+  //   }
+  //   return res.json();
+  // });
+  return axios.get("/api/savedata/load").then((res) => {
+    if (res.status < 200 || res.status >= 300) {
+      throw new Error(`api 에러! status code: ${res.status}`);
     }
-    return res.json();
+    return res.data;
   });
 }
 
