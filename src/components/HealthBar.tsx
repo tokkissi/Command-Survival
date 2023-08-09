88aaff2bcf6b2016b218b17c6f2809904d027465
tokkissi <alal4674@gmail.com>
2023-08-10 00:07:20 +0900

    style: update HealthBar css

--- src/components/HealthBar.tsx
+++ src/components/HealthBar.tsx
@@ -6,14 +6,14 @@ type Props = {
 export default function HealthBar({ currentHP, maxHP }: Props) {
   const hpPercentage = (currentHP / maxHP) * 100;
   return (
-    <div className="flex-col">
-      <div className="w-full h-10 rounded bg-gray-300 border-4 border-yellow-300">
+    <div className="relative flex-col">
+      <div className="w-full h-8 rounded bg-gray-400 border-4 border-yellow-200">
         <div
-          className="h-full bg-green-500 rounded"
+          className="h-full bg-green-600 rounded"
           style={{ width: `${hpPercentage}%` }}
         ></div>
       </div>
-      <div className="whitespace-nowrap font-bold text-xl text-center">
+      <div className="absolute w-full h-full flex flex-col justify-center items-center gap-4 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] whitespace-nowrap font-bold text-base text-center">
         {currentHP} / {maxHP}
       </div>
     </div>
