7f43ae023624f397e30a52acbfc896a33a4ad097
tokkissi <alal4674@gmail.com>
2023-08-18 00:45:41 +0900

    fix: correct handling of escape characters

--- src/app/api/play/route.ts
+++ src/app/api/play/route.ts
@@ -1,4 +1,5 @@
 import { callChatGPT } from "@/service/chatGPT";
+import { formatGptResponse } from "@/util/gptResponseFommatter";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -7,7 +8,15 @@ export async function POST(req: NextRequest) {
   try {
     const response = await callChatGPT({ prompt, conversation });
 
-    return NextResponse.json(JSON.stringify(response?.content), {
+    if (!response || !response.content) {
+      return new Response(
+        JSON.stringify({ error: "Response is null or undefined" }),
+        { status: 500 }
+      );
+    }
+    const content = JSON.stringify(response?.content);
+
+    return NextResponse.json(formatGptResponse(content), {
       status: 200,
     });
   } catch (error) {
