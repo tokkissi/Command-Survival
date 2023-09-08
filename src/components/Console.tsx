fa9e909bb0d3b4736a76bd5bee45249cebf95ae3
tokkissi <alal4674@gmail.com>
2023-09-08 20:42:19 +0900

    feat: add battle logic to Console component

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -8,6 +8,11 @@ import { askAIWithUserInput } from "@/service/conversation";
 import { startSystemPrompt } from "@/Prompt_libaray/startPrompt";
 import { useGameDataStore } from "@/stores/useGameDataStore";
 import { normalEventPrompt } from "@/Prompt_libaray/normalEventPrompt";
+import { useUserData } from "@/stores/useUserData";
+import ModalPortal from "./ui/ModalPortal";
+import InfoModal from "./ui/InfoModal";
+import DetailEndding from "./DetailEndding";
+import { battlePrompt } from "@/Prompt_libaray/battlePrompt";
 
 export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -18,9 +23,26 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
     }[]
   >([]);
   const { isMobile } = useUIStore();
-  const { setGameData } = useGameDataStore();
+  const { setGameData, gameData, incrementFloor } = useGameDataStore();
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [isVictory, setIsVictory] = useState<boolean>(false);
+  const [shouldIncrementFloor, setShouldIncrementFloor] = useState(true);
+
   const flexiblePadding = isMobile ? "" : "p-4";
   const flexibleFontSize = isMobile ? "text-sm" : "text-base";
+
+  const checkChoiceFormat = (text: string): boolean => {
+    // 1. 선택지 1
+    // 2. 선택지 2
+    // 3. 선택지 3
+    // 위와 같은 형식을 가진 문자열이라고 가정
+    const regex = /\d\.\s+.+/g;
+    const matches = text.match(regex);
+    if (matches === null) {
+      return false;
+    }
+    return matches.length >= 1;
+  };
 
   const mutation = useMutation(askAIWithUserInput, {
     onSuccess: (res) => {
@@ -29,6 +51,19 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
       const defaultDef = parseInt(process.env.NEXT_PUBLIC_DEFAULT_DEF!);
       const defaultMaxHP = parseInt(process.env.NEXT_PUBLIC_DEFAULT_MAXHP!);
       const defaultItems = process.env.NEXT_PUBLIC_DEFAULT_ITEMS;
+
+      // 선택지 응답 형식이 없다면 다시 API 호출
+      if (!checkChoiceFormat(res)) {
+        console.log("선택지 불량. 재요청함");
+        setShouldIncrementFloor(false);
+        mutation.mutate({
+          prompt: normalEventPrompt + " 반드시 선택지 3개를 포함해야 해",
+          conversation: [...conversationHistory],
+        });
+        return;
+      }
+
+      setShouldIncrementFloor(true);
 
       // 형식에 맞으면 parsing하여 추출해서 각데이터를 전역 상수로 세팅.
       // 형식에 맞지 않으면 기본값을 세팅
@@ -50,13 +85,44 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
           items: items,
           hp: maxhp,
           maxFloor: Number(process.env.NEXT_PUBLIC_DEFAULT_MAX_FLOOR),
-          currentFloor: 1,
+          currentFloor: gameData.currentFloor,
         });
       }
 
+      // 전투 로그 생성
+      const battleStats = {
+        ATK: gameData.attribute.ATK,
+        DEF: gameData.attribute.DEF,
+        hp: gameData.hp,
+      };
+      let battleLog = "";
+
+      if (gameData.currentFloor !== 0 && gameData.currentFloor % 5 === 0) {
+        battleLog = battlePrompt(battleStats);
+      }
+
+      // 패배 조건을 확인
+      const isDefeated = battleLog.includes("전투 패배!");
+
+      if (isDefeated) {
+        setConversationHistory((preHistory) => [
+          ...preHistory.slice(0, preHistory.length - 1),
+          { role: "assistant", text: battleLog },
+        ]);
+
+        // 2초 후에 handleBattleDefeat 실행
+        setTimeout(() => {
+          handleBattleDefeat();
+        }, 1500);
+        return;
+      }
+
+      // 전투 로그와 GPT API 응답을 합침
+      const combinedResponse = `${battleLog}\n\n${res}`;
+
       setConversationHistory((preHistory) => [
         ...preHistory.slice(0, preHistory.length - 1),
-        { role: "assistant", text: res },
+        { role: "assistant", text: combinedResponse },
       ]);
     },
     onError: (error) => {
@@ -71,6 +137,8 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
     },
   });
 
+  const { incrementCoupon } = useUserData();
+
   useEffect(() => {
     if (isFirstStart) {
       setConversationHistory((preHistory) => [
@@ -83,6 +151,10 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isFirstStart]);
+
+  useEffect(() => {
+    console.log("층 확인", gameData.currentFloor);
+  }, [gameData.currentFloor]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -99,13 +171,33 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
     ]);
     inputRef.current!.value = "";
 
+    let systemContent = "";
+
+    if (shouldIncrementFloor) {
+      incrementFloor();
+    }
+
     mutation.mutate({
       prompt: userInputAsk,
       conversation: [
         ...conversationHistory.slice(-4),
-        { role: "system", text: normalEventPrompt },
+        { role: "system", text: systemContent },
       ],
     });
+  };
+
+  const handleVictory = () => {
+    // 모달로 승리 이미지 보여주고, 유저 쿠폰 수 +1 해주고 보여준 뒤, 타이틀로 리다이렉트
+    incrementCoupon();
+
+    setIsVictory(true);
+    setShowModal(true);
+  };
+
+  const handleBattleDefeat = () => {
+    // 모달로 패배 이미지 보여주고, 타이틀로 리다이렉트
+    setIsVictory(false);
+    setShowModal(true);
   };
 
   return (
@@ -149,6 +241,14 @@ export default function Console({ isFirstStart }: { isFirstStart: boolean }) {
           전송
         </button>
       </form>
+
+      {showModal && (
+        <ModalPortal>
+          <InfoModal onClose={() => setShowModal(false)}>
+            <DetailEndding isVictory={isVictory} />
+          </InfoModal>
+        </ModalPortal>
+      )}
     </div>
   );
 }
